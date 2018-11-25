c698bc89a1d192ff7c4ae0ba13e9a1a5c28357de
Woody Lucas <wlucas0311@gmail.com>
2018-11-25 06:55:57 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -60,13 +60,5 @@ end
 
 def runner
   welcome
-  users_hand = initial_round
-  card_total = users_hand
-  
-  until card_total >= 21
-  card_total = hit>(card_total)
-  display_card_total(card_total)
-end 
-end_game(card_total)
 end
     
