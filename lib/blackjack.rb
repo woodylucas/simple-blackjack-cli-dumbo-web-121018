cefb5290eee7922b4ce1cebc2b89fc846c6250ed
Woody Lucas <wlucas0311@gmail.com>
2018-11-25 05:45:54 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -10,8 +10,8 @@ def display_card_total(total)
   puts "Your cards add up to #{total}"
 end
 
-def prompt_user
- 
+def prompt_user(h, s)
+ puts "Type #{h} to hit or #{s} to stay"
 end
 
 def get_user_input
